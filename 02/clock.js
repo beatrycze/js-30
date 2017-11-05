4c3408fa0387c9e76d0a88a2257157b72f119839
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-11-05 16:22:41 +0100

    Hour hand fix & minute hand improve (day #2)

--- 02/clock.js
+++ 02/clock.js
@@ -17,12 +17,13 @@ function setDate() {
   }
 
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360);
+  const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6);
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 60) * 21600);      
+  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30);
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
+setDate();
 setInterval(setDate, 1000);
