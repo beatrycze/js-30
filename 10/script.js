a6910364d374a33a2cd8fe7e4e3e6e4120eeee83
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-11-11 22:18:53 +0100

    lastItemChecked - improve (day #10)

--- 10/script.js
+++ 10/script.js
@@ -3,12 +3,24 @@ const checkboxes = document.querySelectorAll('.inbox input[type=checkbox]');
 let lastItemChecked;
 
 function handleCheck(event) {
-  // console.log(event);
-  // console.log(this);
+  if (!this.checked) {
+    lastItemChecked = undefined;
+  }
+
+  if(!event.shiftKey && this.checked) {
+    lastItemChecked = this;
+  }
+
+  if (event.shiftKey && this.checked && !lastItemChecked) {
+    let checkedItem = Array.from(checkboxes).find(checkbox => checkbox.checked && checkbox != this);
+    if (checkedItem) {
+      lastItemChecked = checkedItem;
+    }
+  }
 
   let itemBetweenCheckedItems = false;
 
-  if (event.shiftKey && this.checked) {
+  if (event.shiftKey && this.checked && lastItemChecked) {
     checkboxes.forEach(checkbox => {
       if (checkbox === this || checkbox === lastItemChecked) {
         itemBetweenCheckedItems = !itemBetweenCheckedItems;
@@ -19,8 +31,6 @@ function handleCheck(event) {
       }
     });
   }
-
-  lastItemChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
